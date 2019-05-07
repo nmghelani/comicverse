bbb9b1f187f33e0870c3a4db8733e4ed2ee32643
Nevil_d_devil <nevilghelani99612@gmail.com>
2019-05-07 14:04:26 +0530

    Add files via upload

---
+++ Def.js
@@ -0,0 +1,21 @@
+module.exports.defs={
+
+	"mcu":[
+		"MCU is the marvel cinematic universe"
+	],
+	"marvel cinematic universe":[
+		"The Marvel Cinematic Universe is an American media franchise and shared universe that is centered on a series of superhero films, independently produced by Marvel Studios and based on characters that appear in American comic books published by Marvel Comics."
+	],
+	"avengers":[
+		"avengers are the Earth's Mightiest Heroes"
+	],
+	"marvel comics":[
+		"Marvel comics is a comic book but there are some difference between marvel comics and marvel cinematic universe"
+	],
+	"infinity stones":[
+		"Within the Marvel Cinematic Universe, the Infinity Stones are six artifacts that endow their bearers with a variety of powerful abilities. When brought together into a glove known as the Infinity Gauntlet, the Stones give their user the power to reshape all of reality itself. there are 6 infinity stones in MCU"
+	],
+	"shield":[
+	    "Shield is a fictional espionage, special law enforcement, and counter-terrorism agency appearing in American comic books published by Marvel Comics."
+	]
+}
---
+++ role.js
@@ -0,0 +1,197 @@
+module.exports.roles={
+
+	"iron man":[
+		"robert downey junior plays the role of iron man"
+	],
+	"thor":[
+		"chris hemsworth plays the role of thor"
+	],
+	"hulk":[
+		"mark ruffalo plays the role of hulk"
+	],
+	"captain america":[
+		"chris evans plays the role of captain america"
+	],
+	"captain marvel":[
+		"brie larson plays the role of captain marvel"
+	],
+	"black widow":[
+		"scarlett johansson plays the role of black widow"
+	],
+	"war machine":[
+		"don cheadle plays the role of war machine"
+	],
+	"doctor strange":[
+		"benedict cumberbatch plays the role of doctor strange"
+	],
+	"spider man":[
+		"tom holland plays the role of spider man"
+	],
+	"black panther":[
+		"chadwick boseman plays the role of black panther"
+	],
+	"gamora":[
+		"zoe saldana plays the role of gamora"
+	],
+	"hawk eye":[
+	    "Jeremy Renner plays the role of hawkeye"
+	],
+	"nebula":[
+		"karen gillan plays the role of nebula"
+	],
+	"loki":[
+		"tom hiddleston plays the role of loki"
+	],
+	"vision":[
+		"paul bettany plays the role of vision"
+	],
+	"scarlet witch":[
+		"elizabeth olsen plays the role of scarlet witch"
+	],
+	"falcon":[
+		"anthony mackie plays the role of falcon"
+	],
+	"winter soldier":[
+		"sebastian stan plays the role of winter soldier"
+	],
+	"heimdall":[
+		"idris elba plays the role of heimdall"
+	],
+	"okoye":[
+		"danai gurira plays the role of okoye"
+	],
+	"eitri":[
+		"peter dinklage plays the role of eitri"
+	],
+	"wong":[
+		"benedict wong plays the role of wong"
+	],
+	"mantis":[
+		"pom klementieff plays the role of mantis"
+	],
+	"drax":[
+		"dave bautista plays the role of drax"
+	],
+	"groot":[
+		"no one plays the role of groot but the voice of groot is given by vin diesel"
+	],
+	"rocket":[
+		"no one plays the role of rocket but the voice of rocket is given by bradley cooper"
+	],
+	"pepper potts":[
+		"gwyneth paltrow plays the role of pepper potts"
+	],
+	"collector":[
+		"benicio del toro plays the role of collector"
+	],
+	"thanos":[
+		"josh brolin plays the role of thanos"
+	],
+	"star lord":[
+		"chris pratt plays the role of star lord"
+	],
+	"on set rocket":[
+		"seam gunn plays the role of on set rocket"
+	],
+	"secretary of state thaddeus ross":[
+		"willium hurt plays the role of secretary of state thaddeus ross"
+	],
+	"shuri":[
+		"letita wright plays the role of shuri"
+	],
+	"on set groot":[
+		"terry notary plays the role of on set groot"
+	],
+	"ebony maw":[
+		"tom vaughan lawlor plays the role of ebony maw"
+	],
+	"proxima midnight":[
+		"carrie coon plays the role of proxima midnight"
+	],
+	"corvus glaive":[
+		"michael james shaw plays the role of corvus glaive"
+	],
+	"funny old man":[
+		"stan lee plays the role of funny old man"
+	],
+	"mbaku":[
+		"winston duke plays the role of m'baku"
+	],
+	"ayo":[
+		"florence kasumba plays the role of ayo"
+	],
+	"friday":[
+		"noone plays the role of friday but the voice of friday is given by kerry condon"
+	],
+	"on set proxima midnight":[
+		"monique ganderton plays the role of on set proxima midnight"
+	],
+	"ned":[
+		"jacob batalon plays the role of ned"
+	],
+	"cindy":[
+		"tiffany espensen plays the role of cindy"
+	],
+	"sally":[
+		"isabella amara plays the role of sally"
+	],
+	"tiny":[
+		"ethan dizon plays the role of tiny"
+	],
+	"young gamora":[
+		"ariana greenblatt plays the role of young gamora"
+	],
+	"gamorass mother":[
+		"ammenah kaplan plays the role of gamorass mother"
+	],
+	"red skull":[
+		"ross marquand plays the role of red skull"
+	],
+	"secretary ross aide":[
+		"micheal anthony rogers and stephan mcfeely plays the role of secretary ross aide"
+	],
+	"doctor strange double":[
+		"aaron lazar plays the role of doctor strange double"
+	],
+	"thanos reader":[
+		"robert pralgo plays the role of thanos reader"
+	],
+	"teenage groot reader":[
+		"olaniyan thurmon plays the role of teenage groot reader"
+	],
+	"nick fury":[
+		"samual l jackson plays the role of nick fury"
+	],
+	"jabari warrior":[
+		"floyed anthony johns junior plays the role of jabari warrior"
+	],
+	"jack rollins":[
+		"callan mulwey plays the role of jack rollins"
+	],
+	"dora milage":[
+		"marie mouroum plays the role of dora milage"
+	],
+	"maria hill":[
+		"cobie smulders plays the role of maria hill"
+	],
+	"jogger":[
+		"kevin d wilson plays the role of jogger"
+	],
+	"pedestrian":[
+		"i'm sorry. there are too many pedestrians so i can't tell names of all of them"
+	],
+	"worker":[
+		"i'm sorry. there are too many workers so i can't tell names of all of them"
+	],
+	"asgardian":[
+		"i'm sorry. there are too many asgardians so i can't tell names of all of them"
+	],
+	"student":[
+		"i'm sorry. there are too many students so i can't tell names of all of them"
+	],
+	"citizen":[
+		"i'm sorry. there are too many citizens so i can't tell names of all of them"
+	]
+	
+}
+
